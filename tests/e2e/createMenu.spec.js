92bc0ff4284a8d1711a23336716c10621f8ed161
priscilla-cassandra <priscilla-92@live.com>
2026-05-20 13:26:09 +0200

    e2e test for navigation to the home page

---
+++ tests/e2e/createMenu.spec.js
@@ -0,0 +1,13 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("createMenu", () => {
+  test("user can navigate to the home page", async ({ page }) => {
+    await page.goto("http://127.0.0.1:3000/login/");
+
+    await page.getByRole("link", { name: "Home" }).click();
+
+    await expect(
+      page.getByRole("heading", { level: 1, name: "Welcome to this site" }),
+    );
+  });
+});
