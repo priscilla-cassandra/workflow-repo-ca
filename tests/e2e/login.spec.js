7edc6ea745ffa44cde7e79cfb053fcab30993c19
priscilla-cassandra <priscilla-92@live.com>
2026-05-20 10:20:47 +0200

    e2e test for login functionality

---
+++ tests/e2e/login.spec.js
@@ -0,0 +1,18 @@
+import { test, expect } from "@playwright/test";
+import { CONFIG } from "../../config.js";
+
+test.describe("login", () => {
+  test("user can log in", async ({ page }) => {
+    await page.goto(`${CONFIG.apiUrl}auth/login`);
+
+    await page.locator('input[name="email"]').fill(process.env.TEST_USER_EMAIL);
+
+    await page
+      .locator('input[name="password"]')
+      .fill(process.env.TEST_USER_PASSWORD);
+
+    await page.getByRole("button", { name: "Login" }).click();
+
+    await expect(page.getByRole("button", { name: "Logout" })).toBeVisible();
+  });
+});
