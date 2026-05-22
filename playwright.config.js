0993f2785158116595e48eedb4f61b001c66a10a
priscilla-cassandra <priscilla-92@live.com>
2026-05-22 10:35:37 +0200

    commented out webserver, because I want top run npm run dev manually to starte the dev server

--- playwright.config.js
+++ playwright.config.js
@@ -74,9 +74,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: "npm run dev:serve",
-    url: "http://localhost:3000",
-    reuseExistingServer: !process.env.CI,
-  },
+  //webServer: {
+  //command: "npm run env && npm run dev:serve",
+  //url: "http://localhost:3000",
+  //reuseExistingServer: !process.env.CI,
+  //},
 });
